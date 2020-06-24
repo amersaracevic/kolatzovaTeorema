4d978dbb68ab407ea4fa94e453acb1a08a94cdbe
amersaracevic <amer.94np@gmail.com>
2020-06-24 14:11:15 +0200

    Prva verzija

---
+++ kolatzovaTeorema/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolatzovaTeorema
+{
+    class Program
+    {
+        //Proverio da li je paran broj
+        //Ako je paran, delimo sa 2 i nastavljamo
+        //Ako je neparan mnozimo sa 3, dodajemo 1 i nastavljamo
+        //Moramo da stignemo do 1
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj ");
+            int broj = int.Parse(Console.ReadLine());
+            
+
+            while(broj != 1)
+            {
+                if (broj % 2 == 0)
+                {
+                    broj = broj / 2;
+                    Console.WriteLine(broj);
+
+                }
+                else if(broj % 2 == 1)
+                {
+                    broj = broj * 3 + 1;
+                    Console.WriteLine(broj);
+
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
